2afc9ea36e64d4abbf81271ebc11d8f2fb8a78ed
Makerz <makerz03795@hotmail.com>
2024-12-12 14:57:50 +0700

    update: IMU :fire:

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,17 +6,20 @@ describe('template spec', async () => {
 
   it('Open web', () => {
 
-    // Enter a username
-    cy.get('input[name="username"]').type('testuser');
+    cy.contains('h1', 'Kitchen Sink');
 
-    // Enter a password
-    cy.get('input[name="password"]').type('password123');
 
-    // Assert the values entered
-    cy.get('input[name="username"]').should('have.value', 'testuser');
-    cy.get('input[name="password"]').should('have.value', 'password123');
+    // // Enter a username
+    // cy.get('input[name="username"]').type('testuser');
 
-    cy.get('button[name="save"]').click();
+    // // Enter a password
+    // cy.get('input[name="password"]').type('password123');
+
+    // // Assert the values entered
+    // cy.get('input[name="username"]').should('have.value', 'testuser');
+    // cy.get('input[name="password"]').should('have.value', 'password123');
+
+    // cy.get('button[name="save"]').click();
 
   });
 
